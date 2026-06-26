00d08e68e9d68e9b57d86774f6b5cb55f465fba6
zqsd <17475349+zqsd1@users.noreply.github.com>
2026-06-26 17:42:22 +0200

    added +x flag

--- dnsmasq/rootfs/etc/cont-init.d/20-gen-dnsmasq-config.sh
+++ dnsmasq/rootfs/etc/cont-init.d/20-gen-dnsmasq-config.sh
@@ -2,6 +2,11 @@
 # shellcheck shell=bash
 
 #TODO send extra data for /24 to 255.255.255.0
+DHCP_RANGE_MASK=cidr2mask "$(bashio::config 'ip_cidr')"
+
+# jq --arg mask "$DHCP_RANGE_MASK" \
+#   '.netmask = $mask' \
+#   /data/options.json > /tmp/options.json
 
 tempio \
   -conf /data/options.json \
